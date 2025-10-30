2e984d9d2c382967b2be2d84e875d459a97b977f
Yug2704 <hugo27argent@gmail.com>
2025-10-30 09:00:07 +0100

    Update sitemap.xml.ts

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async () => {
-  const base = 'https://tonpseudo.github.io'; // 🔁 ton URL exacte
+  const base = 'https://yug2704.github.io'; // ✅ ton URL exacte
   const urls = ['/', '/category/restaurant'];
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
